8ec37efba1e01681c0f4b789ed126974de8ca009
Piotr Pawlaczyk <piotr.pawlaczyk@student.put.poznan.pl>
2018-01-05 18:01:18 +0100

    Serwer kompresuje plik

---
+++ Code server/server.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <zlib.h>
+#include <zconf.h> 
+#include <fstream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+using namespace std;
+
+//todo adres ip serwera
+int main(){    
+    int socketDescriptor = socket(PF_INET, SOCK_STREAM, 0);
+    	
+    int on = 1;
+    setsockopt(socketDescriptor,SOL_SOCKET, SO_REUSEADDR, (char*) &on, sizeof(on));
+    
+    struct sockaddr_in str;
+    str.sin_family = PF_INET;
+    str.sin_port = htons(12345);//numer portu
+    str.sin_addr.s_addr = INADDR_ANY;
+    
+    bind(socketDescriptor,(struct sockaddr*) &str, sizeof(str));
+    
+    listen(socketDescriptor, 0);
+    
+    //signal(SIGCHLD, SIG_IGN);
+    
+    while(1){
+        struct sockaddr_in newClient;
+        socklen_t size;    
+        
+        int client = accept(socketDescriptor, (struct sockaddr*)&newClient, &size);
+        
+        if (fork() == 0){    
+            close(socketDescriptor);	
+            if(client != -1){
+            	//TODO cały kompunikat = "literka komunikatu(1)||header(100)||reszta pliku(n)"
+                int maxRead = 1;
+                char messageBufforHeader[1];
+                read(client, &messageBufforHeader, 1);
+                char typeMessage = messageBufforHeader[0];
+                switch(typeMessage){
+                    case 'c':{//kompresja
+//WCZYTANIE DANYCH OD KLIENTA                    	
+                        cout<<client<< " Compression: ";
+                        //char bufferToFile[1000];
+                        
+                        char header[100];// = new char(100);
+                        for (int i = 0; i < 100; i++)
+                        {
+                        	char buf[1];
+                        	read(client, &buf, 1);
+                        	//bufferToFile[i] = buf[0];
+                        	header[i] = buf[0];
+                        }
+                        int fileSize = 0;
+                        int start = 0;
+                        for (int i = 86; i <100; i++)//wczytanie rozmiaru pliku wejściowego
+                        {
+                        	int number = header[i] - 48;
+                                cout<<"header[i] " << header[i]<<endl;
+                                cout<<"rozmiar " << number<<endl;
+                                
+                        	if (number != 0)
+                        	{
+                        		start = 1;
+                        	}
+                        	if (start == 1)
+                        	{
+                        		fileSize = fileSize*10 + number;	
+                        	}
+                        }
+                        cout<<"file size : "<<fileSize<<endl;
+                        
+                        cout<<"Doczytuje dane " <<endl;
+                        char * bufferToFile = new char[fileSize];
+                        for (int i=0 ; i<100 ; i++) {
+                            bufferToFile[i]=header[i];
+                            
+                        }
+                        //delete(header);
+                      int actualByte = 100;
+                        char tmpBufforForData[maxRead];
+                        do{                                 // wczytywanie pliku ze strumienia
+                            
+                           int readed = read(client, &tmpBufforForData, maxRead);
+                           for (int i = 0; i < readed; i++)//przepisanie wczytanych plików
+                           {
+                           		bufferToFile[actualByte] = tmpBufforForData[i];
+                           		actualByte++;
+                                     //   cout<<"actualByte" << actualByte<<endl;
+                           }
+                        }while(actualByte != fileSize);
+                        cout<<"Wczytałem cały strumień wejściowy."<<endl;
+//KOMPRESJA
+                        cout<<"Rozpoczynam kompresję"<<endl;
+                     
+                        
+                        char* compressFileBuffer = new char[(int)(fileSize*1.1 + 12)];
+                                        
+    					unsigned long lenghtAfterCompress; 
+                                       
+                                        
+					    //kompresujemy z najlepsza metoda
+					    compress2((Bytef *)compressFileBuffer,&lenghtAfterCompress,
+					    (const  Bytef*)bufferToFile,fileSize,Z_BEST_COMPRESSION);
+                                          cout<<"rxxx"<<endl;
+					    string fileName = string(header,100)+".zlib";
+                                                
+    					//tworzenie pliku w ktorym zapiszemy zpakowane dane
+    					fstream fileToCompress;
+    					fileToCompress.open(fileName.c_str(),ios::binary|ios::out);
+					 
+					    for(unsigned int i = 0; i<lenghtAfterCompress;i++)
+					    {
+					        fileToCompress.write((char*)&compressFileBuffer[i],1);  
+					    }
+ 						fileToCompress.close();
+ 						cout<<client<<" : "<<"skończyłem kompresję";
+                                                
+                        delete(compressFileBuffer);
+                        delete(bufferToFile);
+                        break;
+//KONIEC KOMPRESJI
+                    }
+                    case 'd':{//dekompresja
+                     
+                        break;
+                    }
+                    case 'w':{//wyświetl listę 
+                     
+                        break;
+                    }
+
+
+                }
+                
+                // do{//czytanie wiadomości
+                //     read(client, &bufforToRead, maxRead);
+                //     printf("%c", bufforToRead[0]);
+                //     message[j] = bufforToRead[0];
+                //     j += i;
+                // }while ( bufforToRead[0] != '\n');
+                // printf("%s\n", message);
+                // if(strcmp(message,"127228\n") == 0){
+                    
+                //     write(client, buff, sizeof(buff));
+                // }else if(strcmp(message,"127265\n")== 0){
+                //     char buff[24];
+                //     sprintf(buff, "JAROSLAW SKRZYPCZAK %d\n", i);
+                //     printf("%s\n", buff);
+                //     write(client, buff, sizeof(buff));
+                // }else{
+                // char buff[20];
+                // sprintf(buff, "I DONT KNOW YOU %d\n", i);
+                // printf("%s\n", buff);
+                // write(client, buff, sizeof(buff));
+                // }
+            }
+            printf("connect: %s\n", inet_ntoa((struct in_addr) newClient.sin_addr));
+            exit(0);
+        }
+        close(client);
+    }
+
+
+
+
+	return 0;
+}
