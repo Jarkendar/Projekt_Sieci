e88537604c2162f6ce3f9c9d80e5349b4b5195b5
jaroslaw <jarek.skrzypczak@o2.pl>
2018-01-07 11:24:05 +0100

    Add basic click listeners

--- Client_App/src/sample/Controller.java
+++ Client_App/src/sample/Controller.java
@@ -1,7 +1,9 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+
 
 public class Controller {
     public Button buttonSend;
@@ -9,4 +11,24 @@ public class Controller {
     public Button buttonDownload;
     public Button buttonChooseFile;
     public ListView listFiles;
+
+    public void clickChooseFile(ActionEvent actionEvent) {
+        actionEvent.getSource();
+        System.out.println(actionEvent);
+    }
+
+    public void clickSend(ActionEvent actionEvent) {
+        actionEvent.getSource();
+        System.out.println(actionEvent);
+    }
+
+    public void clickList(ActionEvent actionEvent) {
+        actionEvent.getSource();
+        System.out.println(actionEvent);
+    }
+
+    public void clickDownload(ActionEvent actionEvent) {
+        actionEvent.getSource();
+        System.out.println(actionEvent);
+    }
 }
