cf01f212bcdb02c12daea4d01ee52d0bdda877a5
jaroslaw <jarek.skrzypczak@o2.pl>
2018-01-07 12:49:27 +0100

    Add get item from list

--- Client_App/src/sample/Controller.java
+++ Client_App/src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -10,25 +12,54 @@ public class Controller {
     public Button buttonList;
     public Button buttonDownload;
     public Button buttonChooseFile;
-    public ListView listFiles;
+    public ListView<String> listFiles;
 
     public void clickChooseFile(ActionEvent actionEvent) {
         actionEvent.getSource();
         System.out.println(actionEvent);
+        //TODO get file path
     }
 
     public void clickSend(ActionEvent actionEvent) {
         actionEvent.getSource();
         System.out.println(actionEvent);
+        //TODO send data to server
     }
 
     public void clickList(ActionEvent actionEvent) {
         actionEvent.getSource();
         System.out.println(actionEvent);
+        //TODO send request for list
+        //TODO receive list and decode his
+        int size = 100;
+        String array[] = new String[size];
+        for (int i = 1; i<=size; i++){
+            array[i-1] = "line "+i;
+        }
+        prepareDataToListView(array);
     }
 
     public void clickDownload(ActionEvent actionEvent) {
         actionEvent.getSource();
         System.out.println(actionEvent);
+        if (listFiles.getSelectionModel().getSelectedItem() != null){
+            String lines = listFiles.getSelectionModel().getSelectedItem();
+            System.out.println(lines);
+        }else {
+            System.out.println("Not choose file from list.");
+        }
+        //TODO send request with file name from list
+        //TODO download stream
+        //TODO decode stream and save file
     }
+
+    public void prepareDataToListView(String lines[]){
+        ObservableList<String> listTest = FXCollections.observableArrayList(lines);
+        setListText(listTest);
+    }
+
+    private void setListText(ObservableList<String> list){
+        listFiles.setItems(list);
+    }
+
 }
